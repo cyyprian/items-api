3d7b6007faec77b8b370534e4195c7a9fc464b10
Ciprian Barbu <isciprian@icloud.com>
2022-09-09 07:14:46 +0300

    Show errors within form

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -9,6 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <!-- Validation Errors -->
+                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                    
                     <form method="POST" action="{{ route('items.store') }}" enctype="multipart/form-data">
                         @csrf
 
