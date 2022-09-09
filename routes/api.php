5444f8b1dc1fd4eeff53c3c77d53c9dba11f9022
Ciprian Barbu <isciprian@icloud.com>
2022-09-09 10:16:25 +0300

    LocalOnly middleware

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/items', [ItemsApiController::class, 'items']);
+Route::get('/items', [ItemsApiController::class, 'items'])->middleware('localonly');
